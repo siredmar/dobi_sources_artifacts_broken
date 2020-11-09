47405f63f61a044983c48e5f32029b15dbbaa39d
Armin Schlegel <armin.schlegel@gmx.de>
2020-11-09 10:29:44 +0100

    Added minimal example for broken sources, artifact in dobi

---
+++ source/lib/lib.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "lib.h"
+
+void Foo() 
+{
+    std::cout << "Foo from lib\n" << std::endl;
+}
---
+++ source/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "lib/lib.h"
+int main()
+{
+  std::cout << "Hello world!\n" << std::endl;
+  Foo();
+  return 0;
+}
